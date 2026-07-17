a31b28006dbc56c328cb4b7a42f69fa0f33eef42
OmniNode <nikolegocabrera@gmail.com>
2026-07-17 12:36:02 -0400

    -fixed build error and removed packages

--- src/kernel/shell/packages.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/* ============================================================
-   OmniOS v1.0.0 - Package Manager Header
-   Included by packages.c, referenced from shell.h
-   ============================================================ */
-
-/* All declarations are in shell.h */
